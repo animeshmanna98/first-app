8c9bfac5deee3c419a3deb614e777bfed5737f99
Symlinktech <symlinktech@gmail.com>
2022-11-27 21:19:23 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const loadData = async () => {
+    try {
+     const response = await fetch('http://sangita.iosx.in:9000/api/user-search',{
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: {
+       "keyword": ""
+      }
+     });
+     const json = await response.json();
+     setData(json.data);
+   } catch (error) {
+     console.error(error);
+   } finally {
+     setLoading(false);
+   }
+  }
+  
+  useEffect(()=>{
+  loadData();
+  },[]);
+
+  const DATA = data;
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
     </View>
+  );
+  const renderItem = ({ item }) => (
+    <Item title={item.name} />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+    <FlatList
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={item => item._id}
+    />
+  </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
